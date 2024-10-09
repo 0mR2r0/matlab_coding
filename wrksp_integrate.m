3c7f348b81611ca26e414c439b680a038d0c1b16
0mR2r0 <omarturo01@gmail.com>
2024-10-09 15:44:54 -0600

    Solucion de sistema usando simulink con Matlab 2024

--- wrksp_integrate.m
+++ wrksp_integrate.m
@@ -2,55 +2,57 @@ clear
 clc
 %Workspace para integrar un sistema
 L=1;
-n=8;
+n=20;
 k=10;
-h=L/n;
+h=L/(n/2);
 a=1/(2*h);
 b=1/(h^2);
 c=(2+k*h^2)/(h^2);
 A=zeros(n,n);
+m=n/2;
 %Ciclo para llenado de coeficientes de una matriz
         for i=1:n
-            if  i<(n/2)+1
+            if  i< m+1
                 if i==1
                     A(i,i)   = -k;
-                    A(i,i+1) = a;
-                    A(i,i+4) = k;
-                elseif i==4
-                    A(i,i-1)   = -a;
-                    A(i,i)   =  -k;
-                    A(i,i+4) = k;
-                elseif i<4
+                    A(i,i+1) =  a;
+                    A(i,i+m) =  k;
+                elseif i==m
                     A(i,i-1) = -a;
                     A(i,i)   = -k;
-                    A(i,i+1) = a;
-                    A(i,i+4) = k;
+                    A(i,i+m) =  k;
+                elseif i<m
+                    A(i,i-1) = -a;
+                    A(i,i)   = -k;
+                    A(i,i+1) =  a;
+                    A(i,i+m) =  k;
                 end
             else
-                if  i==(n/2)+1
-                    A(i,1)   = k;
-                    A(i,i) = -c;
-                    A(i,i+1) = b;
+                if  i==m+1
+                    A(i,1)   =  k;
+                    A(i,i)   = -c;
+                    A(i,i+1) =  b;
+                    
                 elseif i==n
-                    A(i,i-1)   = b;
-                    A(i,i)   =  -c;
-                    A(i,n/2) = k;
-                elseif i>(n/2)+1
+                    A(i,i-1) =  b;
+                    A(i,i)   = -c;
+                    A(i,m)   =  k;
+                elseif i>(m)+1
                     A(i,i-1) = b;
                     A(i,i)   = -c;
                     A(i,i+1) = b;
-                    A(i,i-4) = k;
+                    A(i,i-m) = k;
                 end    
             end
         end
 x0_1=zeros(1,4);
 x0_2=zeros(1,4);
         count=0;
-        for i=1:4  %Vector de condiciones iniciales para U1(0)
+        for i=1:m  %Vector de condiciones iniciales para U1(0)
             count=count+h;
             x0_1(1,i)=exp(-100*(count-0.5)^2);
         end   
-        for i=1:4  %Vector de condiciones iniciales para U2(0)
+        for i=1:m  %Vector de condiciones iniciales para U2(0)
             x0_2(1,i)=0;
         end
         ci=[x0_1,x0_2];
